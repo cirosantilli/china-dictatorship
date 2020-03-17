1ce4a4dff42a91b36a85c9e238fd3d5eeb97b965
Ciro Santilli <ciro.santilli@gmail.com>
2020-03-17 00:00:09 +0000

    python package sketch

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='china-dictatorship',
+    version='0.0.1',
+    description='2018新疆改造中心，1989六四事件，1999法轮功 ，2019 996.ICU, 2018包子露宪，2015 710律师劫，2015巴拿马文件 邓家贵，2017低端人口，2008西藏骚乱',
+    long_description='2018新疆改造中心，1989六四事件，1999法轮功 ，2019 996.ICU, 2018包子露宪，2015 710律师劫，2015巴拿马文件 邓家贵，2017低端人口，2008西藏骚乱',
+    url='https://github.com/cirosantilli/china-dictatorship',
+    author='Ciro Santilli',
+    author_email='ciro.santilli.contact@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['china-dictatorship.py'],
+)
