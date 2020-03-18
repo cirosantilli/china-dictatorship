e7e446080edd57905a13fb79114b3b6b4539247f
Ciro Santilli <ciro.santilli@gmail.com>
2020-03-18 00:00:10 +0000

    bump package version

--- setup.py
+++ setup.py
@@ -9,13 +9,19 @@ import json
 
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.adoc')) as f:
+    long_description = f.read()
+
 setup(
     name='china-dictatorship',
-    version='0.0.23',
+    version='0.0.27',
     description='2018新疆改造中心，1989六四事件，1999法轮功 ，2019 996.ICU, 2018包子露宪，2015 710律师劫，2015巴拿马文件 邓家贵，2017低端人口，2008西藏骚乱',
-    # Fails with: "The description failed to render in the default format of reStructuredText."
-    #long_description=readme(),
-    long_description='2018新疆改造中心，1989六四事件，1999法轮功 ，2019 996.ICU, 2018包子露宪，2015 710律师劫，2015巴拿马文件 邓家贵，2017低端人口，2008西藏骚乱',
+    long_description=long_description,
+    # Otherwise upload fails with: "The description failed to render in the default format of reStructuredText."
+    long_description_content_type='text/plain',
+    # long_description='2018新疆改造中心，1989六四事件，1999法轮功 ，2019 996.ICU, 2018包子露宪，2015 710律师劫，2015巴拿马文件 邓家贵，2017低端人口，2008西藏骚乱',
     url='https://github.com/cirosantilli/china-dictatorship',
     author='Ciro Santilli',
     author_email='ciro.santilli.contact@gmail.com',
