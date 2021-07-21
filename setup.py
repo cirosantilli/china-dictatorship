54a5acf0c19b345ad4a49903b45c3bee73138bfa
Ciro Santilli <ciro.santilli@gmail.com>
2021-07-21 18:52:46 +0100

    bump package version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(this_directory, 'README.adoc')) as f:
 
 setup(
     name='china-dictatorship',
-    version='0.0.71',
+    version='0.0.72',
     description='2018新疆改造中心，1989六四事件，1999法轮功 ，2019 996.ICU, 2018包子露宪，2015 710律师劫，2015巴拿马文件 邓家贵，2017低端人口，2008西藏骚乱',
     long_description=long_description,
     # Otherwise upload fails with: "The description failed to render in the default format of reStructuredText."
