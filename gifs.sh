29b91a18a5bfcaf797388de57cf42443c984545b
Ciro Santilli <ciro.santilli@gmail.com>
2021-08-18 11:14:24 +0100

    grammar fixes, gifs download

---
+++ gifs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+wget -O mma.gif https://na.cx/i/ubREcE9.gif
+wget -O bruce-lee.gif https://upload.hkgolden.media/comment/epoean0x.wxbwjfsfvvq.fiu020yfszj.15h.gif
+wget -O man-slap.gif https://na.cx/i/1CQ5O5G.gif
+wget -O bouncing-with-lam.gif https://na.cx/i/k1nOTYo.gif
+wget -O hong-kong-injection.gif https://na.cx/i/mDVT444.gif
+wget -O doctor-thumb.gif https://na.cx/i/0vYFXZP.gif
+wget -O lam-student-slap.gif https://na.cx/i/1beam86.gif
+wget -O laugh-old.gif https://na.cx/i/PcBtpWT.gif
+wget -O gangster-laugh.gif https://na.cx/i/4KzLOrD.gif
+wget -O hulk.gif https://na.cx/i/54rgZka.gif
+wget -O tear-shirt.gif https://na.cx/i/s46NPZz.gif
